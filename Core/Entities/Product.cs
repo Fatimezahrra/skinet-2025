587ab9137dcb5907c68aacd52069c2423a3cb470
Fatima Zohra <fbakhatar@gmail.com>
2025-12-23 15:24:00 +0100

    End of section 2

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Entities;
+
+/// <summary>
+/// Summary description for Product
+/// </summary>
+public class Product
+{
+
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public required string PictureUrl { get; set; }
+    public required string Type { get; set; }
+    public required string Brand { get; set; }
+    public int QuantityInStock { get; set; }
+
+}
